41169d12b50ba07956a31a237cf44f1515ec7726
Jonah Redd <youness.echchadi@gmail.com>
2016-09-01 07:13:27 +0100

    working app on 31 august

---
+++ index.js
@@ -0,0 +1,38 @@
+navigator.mozGetUserMedia({video:true, audio:false}, function(stream){
+
+    var Peer = require('simple-peer')
+    var peer = new Peer({
+        initiator: location.hash === '#init',
+        trickle: false,
+        stream: stream
+    })
+
+    peer.on('signal', function (data) {
+        document.getElementById('yourID').value = JSON.stringify(data)
+    }) 
+
+    document.getElementById('connect').addEventListener('click', function() {
+        var otherID = JSON.parse(document.getElementById('otherID').value)
+        peer.signal(otherID)
+    })
+
+    document.getElementById('send').addEventListener('click', function() {
+        var yourMessage = document.getElementById('yourMessage').value
+        peer.send(yourMessage)
+    })
+
+    peer.on('data', function (data) {
+        document.getElementById('messages').textContent += data + '\n'
+    })
+
+    peer.on('stream', function(stream){
+        var video = document.createElement('video')
+        document.body.appendChild(video)
+
+        video.src = window.URL.createObjectURL(stream)
+        video.play()
+    })
+
+}, function(err){
+    console.error(err)
+})
